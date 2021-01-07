d80df9ccc6d116a3df93c37ca5c05a9d93fb94a0
Piotr Slezak <piotrek.slezak@gmail.com>
2021-01-07 16:03:40 +0100

    prepare array for different api keys

--- my-app/src/CityWeatherComponent.js
+++ my-app/src/CityWeatherComponent.js
@@ -4,11 +4,12 @@ export default function ShowWeatherInCity() {
     const [temperature, setTemperature] = useState(0);
     const [city, setCity] = useState(0);
 
+    // const apiKeys = []; array z różnymi api keys, które potem są losowane do funkcji fetch celem upewnienia się, że nie ma za dużo requestów
+
     useEffect(() => {
         fetch('http://api.openweathermap.org/data/2.5/weather?q=Zawory,pl&appid=818c22ea71e55d4533a6c533966f8bcb&units=metric')
             .then((response) => response.json())
             .then((data) => {
-                console.log('This is your data', data);
                 setTemperature(data.main.temp);
                 setCity(data.name);
             });
@@ -16,7 +17,7 @@ export default function ShowWeatherInCity() {
 
     return (
         <div>
-            Miasto: {city} <br/>
+            Miasto: {city} <br />
             Temperatura: {temperature} stopni Celsjusza
         </div>
     );
