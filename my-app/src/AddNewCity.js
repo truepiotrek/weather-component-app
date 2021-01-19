a83a244891f4fa475ddc0722fbb4db8ff9a8f09e
Piotr Slezak <piotrek.slezak@gmail.com>
2021-01-19 14:18:22 +0100

    add button for input handling in AddNewCity component

--- my-app/src/AddNewCity.js
+++ my-app/src/AddNewCity.js
@@ -1,16 +1,33 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import axios from "axios";
 import './AddNewCity.css';
 
-export default function AddNewCityToList() {
+export default function AddNewCityToList(props) {
 
-    const [input, setInput] = useState("np. Białystok")
+    const [input, setInput] = useState("")
+
+    function handleInputChange(event){
+        setInput(event.target.value);
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        if(input === ''){
+            console.log('Nie podano miasta, uzupełnij proszę')
+        } else {
+            if (typeof props.onAdd === 'function') {
+                props.onAdd(input);
+            }
+            setInput('');
+        }
+    }
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label for="city-name">Dodaj miasto</label>
-                <input type="text" id="city-to-add" /><br />
+                <input type="text" id="city-to-add" value={input} onChange={handleInputChange}/><br />
+                <input type="button" value="Dodaj" className="button-add" onClick={handleSubmit}/>
             </form>
         </div>
     );
